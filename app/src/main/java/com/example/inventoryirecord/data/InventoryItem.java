81bf63405c4cc9ad3d00d19e011147e91026af78
DanielDomme <dommed@oregonstate.edu>
2020-02-20 15:45:21 -0800

    add inventory item base object with builder

---
+++ app/src/main/java/com/example/inventoryirecord/data/InventoryItem.java
@@ -0,0 +1,131 @@
+package com.example.inventoryirecord.data;
+
+import java.util.ArrayList;
+
+public class InventoryItem {
+    private String itemName;
+    private String dateAdded;
+    private String datePurchased;
+    private String make;
+    private String model;
+    private String serialNumber;
+    private String dateOfManufacture;
+    private String otherNotes;
+    private boolean newItem;
+    private double pricePaid;
+    private double value;
+    //image location??
+    private ArrayList<String> itemPics;
+    private ArrayList<String> receiptPics;
+
+    //to use the builder : InventoryItem inventoryItem =
+    // InventoryItem.Builder.newInstance().setItemName("name").build();
+
+    public InventoryItem(Builder builder) {
+        this.itemName = builder.itemName;
+        this.dateAdded = builder.dateAdded;
+        this.datePurchased = builder.datePurchased;
+        this.make = builder.make;
+        this.model = builder.model;
+        this.serialNumber = builder.serialNumber;
+        this.dateOfManufacture = builder.dateOfManufacture;
+        this.otherNotes = builder.otherNotes;
+        this.newItem = builder.newItem;
+        this.pricePaid = builder.pricePaid;
+        this.value = builder.value;
+        this.itemPics = builder.itemPics;
+        this.receiptPics = builder.receiptPics;
+    }
+
+    public static class Builder {
+        private String itemName;
+        private String dateAdded;
+        private String datePurchased;
+        private String make;
+        private String model;
+        private String serialNumber;
+        private String dateOfManufacture;
+        private String otherNotes;
+        private boolean newItem;
+        private double pricePaid;
+        private double value;
+        //image location??
+        private ArrayList<String> itemPics;
+        private ArrayList<String> receiptPics;
+
+        public static Builder newInstance() {
+            return new Builder();
+        }
+        private Builder() {
+        }
+
+        public InventoryItem build(){
+            return new InventoryItem(this);
+        }
+
+        public Builder setItemName(String name) {
+            this.itemName = name;
+            return this;
+        }
+
+        public Builder setDateAdded(String dateAdd) {
+            this.dateAdded = dateAdd;
+            return this;
+        }
+
+        public Builder setDatePurchased(String dateOfPurchase) {
+            this.datePurchased = dateOfPurchase;
+            return this;
+        }
+
+        public Builder setMake(String brandName) {
+            this.make = brandName;
+            return this;
+        }
+
+        public Builder setModel(String itemModel) {
+            this.model = itemModel;
+            return this;
+        }
+
+        public Builder setSerialNumber(String sn) {
+            this.serialNumber = sn;
+            return this;
+        }
+
+        public Builder setDateOfManufacture(String dateMade) {
+            this.dateOfManufacture = dateMade;
+            return this;
+        }
+
+        public Builder setNewItem(boolean itemNew) {
+            this.newItem = itemNew;
+            return this;
+        }
+
+        public Builder setOtherNotes(String notes) {
+            this.otherNotes = notes;
+            return this;
+        }
+
+        public Builder setPricePaid(Double paid) {
+            this.pricePaid = paid;
+            return this;
+        }
+
+        public Builder setValue(Double value) {
+            this.value = value;
+            return this;
+        }
+
+        public Builder setItemPics(ArrayList<String> pics) {
+            this.itemPics = pics;
+            return this;
+        }
+
+        public Builder setReceiptPics(ArrayList<String> receipts) {
+            this.receiptPics = receipts;
+            return this;
+        }
+    }
+}
